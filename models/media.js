d4f0241a9d52fb9525efcfcfcc54ef5a3cd4b950
NickLauf2022 <111160103+NickLauf2022@users.noreply.github.com>
2022-08-13 16:39:30 -0500

    Add files via upload

---
+++ models/media.js
@@ -0,0 +1,10 @@
+const video = {
+  id: "video-media",
+  src: "images/lighthouse.ogv",
+};
+const audio = {
+  id: "audio-media",
+  src: "images/podcast.mp3",
+};
+
+module.exports = { video, audio };
